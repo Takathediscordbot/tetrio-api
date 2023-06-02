afe1260fe9a6495d73f1324f162203d8408565c7
Taka <katamithetaka@gmail.com>
2023-06-02 14:09:22 +0200

    add Eq to enum UserRank

--- src/models/users/user_rank.rs
+++ src/models/users/user_rank.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
 #[serde(rename_all = "lowercase")]
 pub enum UserRank {
     X,
