e901d18f8a6c0ab3ee32e260a2c9309e24a9f659
Taka <katamithetaka@gmail.com>
2023-06-02 23:24:20 +0200

    update moka package

--- src/http/cached_client.rs
+++ src/http/cached_client.rs
@@ -296,7 +296,7 @@ impl CachedClient {
     /// use tetrio_api::http::cached_client::CachedClient;
     /// # tokio_test::block_on(async {
     /// let client = CachedClient::default();
-    /// let packet = client.fetch_full_league_leaderboard(Some("fr")).await.unwrap();
+    /// let packet = client.fetch_full_league_leaderboard(Some("fr".to_string())).await.unwrap();
     /// 
     /// assert!(packet.success && packet.data.is_some() && packet.error.is_none());
     /// 
