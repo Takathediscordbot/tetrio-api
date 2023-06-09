562da7b546ad54d564e0cf8df5b7842c572fdacf
Taka <katamithetaka@gmail.com>
2023-06-09 19:29:20 +0200

    update userdistinguishment

--- src/models/users/user_distinguishment.rs
+++ src/models/users/user_distinguishment.rs
@@ -3,5 +3,8 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct UserDistinguishment {
     #[serde(rename = "type")]
-    pub identifier: String,
+    pub distinguishment_type: String,
+    pub detail: Option<String>,
+    pub header: Option<String>,
+    pub footer: Option<String>
 }
